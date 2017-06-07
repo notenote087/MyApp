21059f04625177aed3f9ee01d56f15c6ace6905a
notenote087 <nonthawat.ro.57@ubu.ac.th>
2017-06-07 15:57:51 +0700

    OK HomeWork

--- app/src/main/java/com/example/admin/myapp/MainActivity.java
+++ app/src/main/java/com/example/admin/myapp/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.admin.myapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import static android.R.attr.duration;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);  // grgrgr system.out.print;
         setContentView(R.layout.activity_main);
+    }
+
+    public  void showText(View view) {
+        String msg = "Hello Teacher Tar I'm Handsome !";
+        Toast toast  = Toast.makeText(this, msg, Toast.LENGTH_SHORT); // toast is Alert
+        toast.show();
     }
 }
