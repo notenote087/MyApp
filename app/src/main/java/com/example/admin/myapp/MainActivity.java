562553a8440655f9aeb54ea91676267637144c0e
notenote087 <nonthawat.ro.57@ubu.ac.th>
2017-06-07 14:42:29 +0700

    OK

---
+++ app/src/main/java/com/example/admin/myapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.admin.myapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);  // grgrgr system.out.print;
+        setContentView(R.layout.activity_main);
+    }
+}
